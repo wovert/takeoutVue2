b7fcf657d633140ddfd09ab431771b192940d07e
wovert <67668283@qq.com>
2019-06-23 13:03:23 +0800

    init project

---
+++ README.md
@@ -0,0 +1,29 @@
+# 饿了吗 webapp
+
+## 环境搭建
+
+- Node 8.9以上
+
+```sh
+vue -h
+vue create vue-sell-cube
+  Babel
+  CSS Pre-processors
+  Linter / Formatter
+  Vuex
+  Router
+cd vue-sell-cube
+yarn serve
+```
+
+## 安装cube第三方模块
+
+插件方式安装cube-ui
+```sh
+vue add cube-ui
+  Use post-compile? 后编译，指的是使用源码
+```
+
+## api 接口 mock
+
+
---
+++ vue-sell-cube/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <cube-button>11</cube-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
